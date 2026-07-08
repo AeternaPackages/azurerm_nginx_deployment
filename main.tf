1b659884720c4b8856d6ff55cbca36d6d312daa3
Daniel Allen <dan@aeternagroup.net>
2026-07-08 23:40:58 +0100

    Update to schema version v4.80.0

---
+++ main.tf
@@ -0,0 +1,54 @@
+locals {
+  nginx_deployments = { for k1, v1 in var.nginx_deployments : k1 => { auto_scale_profile = v1.auto_scale_profile, automatic_upgrade_channel = v1.automatic_upgrade_channel, capacity = v1.capacity, diagnose_support_enabled = v1.diagnose_support_enabled, email = v1.email, frontend_private = v1.frontend_private, frontend_public = v1.frontend_public, identity = v1.identity, location = v1.location, logging_storage_account = v1.logging_storage_account, managed_resource_group = v1.managed_resource_group, name = v1.name, network_interface = v1.network_interface, resource_group_name = v1.resource_group_name, sku = v1.sku, tags = v1.tags, web_application_firewall = v1.web_application_firewall } }
+
+  nginx_api_keys = merge([
+    for k1, v1 in var.nginx_deployments : {
+      for k2, v2 in coalesce(v1.nginx_api_keys, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        nginx_deployment_id = module.nginx_deployments.nginx_deployments["${k1}"].id
+      })
+    }
+  ]...)
+
+  nginx_certificates = merge([
+    for k1, v1 in var.nginx_deployments : {
+      for k2, v2 in coalesce(v1.nginx_certificates, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        nginx_deployment_id = module.nginx_deployments.nginx_deployments["${k1}"].id
+      })
+    }
+  ]...)
+
+  nginx_configurations = merge([
+    for k1, v1 in var.nginx_deployments : {
+      for k2, v2 in coalesce(v1.nginx_configurations, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        nginx_deployment_id = module.nginx_deployments.nginx_deployments["${k1}"].id
+      })
+    }
+  ]...)
+}
+
+module "nginx_deployments" {
+  source            = "git::https://github.com/AeternaModules/azurerm_nginx_deployment.git?ref=v4.80.0"
+  nginx_deployments = local.nginx_deployments
+}
+
+module "nginx_api_keys" {
+  source         = "git::https://github.com/AeternaModules/azurerm_nginx_api_key.git?ref=v4.80.0"
+  nginx_api_keys = local.nginx_api_keys
+  depends_on     = [module.nginx_deployments]
+}
+
+module "nginx_certificates" {
+  source             = "git::https://github.com/AeternaModules/azurerm_nginx_certificate.git?ref=v4.80.0"
+  nginx_certificates = local.nginx_certificates
+  depends_on         = [module.nginx_deployments]
+}
+
+module "nginx_configurations" {
+  source               = "git::https://github.com/AeternaModules/azurerm_nginx_configuration.git?ref=v4.80.0"
+  nginx_configurations = local.nginx_configurations
+  depends_on           = [module.nginx_deployments]
+}
+
---
+++ variables.tf
@@ -0,0 +1,115 @@
+variable "nginx_deployments" {
+  description = <<EOT
+Map of nginx_deployments, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+    - sku
+Optional:
+    - automatic_upgrade_channel
+    - capacity
+    - diagnose_support_enabled
+    - email
+    - managed_resource_group
+    - tags
+    - auto_scale_profile (block)
+    - frontend_private (block)
+    - frontend_public (block)
+    - identity (block)
+    - logging_storage_account (block)
+    - network_interface (block)
+    - web_application_firewall (block)
+Nested nginx_api_keys (azurerm_nginx_api_key):
+    Required:
+        - end_date_time
+        - name
+        - secret_text
+Nested nginx_certificates (azurerm_nginx_certificate):
+    Required:
+        - certificate_virtual_path
+        - key_vault_secret_id
+        - key_virtual_path
+        - name
+Nested nginx_configurations (azurerm_nginx_configuration):
+    Required:
+        - root_file
+    Optional:
+        - package_data
+        - config_file (block)
+        - protected_file (block)
+EOT
+
+  type = map(object({
+    location                  = string
+    name                      = string
+    resource_group_name       = string
+    sku                       = string
+    automatic_upgrade_channel = optional(string) # Default: "stable"
+    capacity                  = optional(number)
+    diagnose_support_enabled  = optional(bool)
+    email                     = optional(string)
+    managed_resource_group    = optional(string)
+    tags                      = optional(map(string))
+    auto_scale_profile = optional(object({
+      max_capacity = number
+      min_capacity = number
+      name         = string
+    }))
+    frontend_private = optional(object({
+      allocation_method = string
+      ip_address        = string
+      subnet_id         = string
+    }))
+    frontend_public = optional(object({
+      ip_address = optional(list(string))
+    }))
+    identity = optional(object({
+      identity_ids = optional(set(string))
+      type         = string
+    }))
+    logging_storage_account = optional(object({
+      container_name = optional(string)
+      name           = optional(string)
+    }))
+    network_interface = optional(object({
+      subnet_id = string
+    }))
+    web_application_firewall = optional(object({
+      activation_state_enabled = bool
+    }))
+    nginx_api_keys = optional(map(object({
+      end_date_time = string
+      name          = string
+      secret_text   = string
+    })))
+    nginx_certificates = optional(map(object({
+      certificate_virtual_path = string
+      key_vault_secret_id      = string
+      key_virtual_path         = string
+      name                     = string
+    })))
+    nginx_configurations = optional(map(object({
+      root_file    = string
+      package_data = optional(string)
+      config_file = optional(object({
+        content      = string
+        virtual_path = string
+      }))
+      protected_file = optional(object({
+        content      = string
+        virtual_path = string
+      }))
+    })))
+  }))
+
+  validation {
+    condition = alltrue(concat(
+      [for kk in keys(var.nginx_deployments) : !strcontains(kk, "/")],
+      flatten([for k0, v0 in var.nginx_deployments : [for kk in keys(coalesce(v0.nginx_api_keys, {})) : !strcontains(kk, "/")]]),
+      flatten([for k0, v0 in var.nginx_deployments : [for kk in keys(coalesce(v0.nginx_certificates, {})) : !strcontains(kk, "/")]]),
+      flatten([for k0, v0 in var.nginx_deployments : [for kk in keys(coalesce(v0.nginx_configurations, {})) : !strcontains(kk, "/")]])
+    ))
+    error_message = "Map keys in this package must not contain '/': it is used internally as a nesting-key separator, so a key containing it can silently collide two different nested entries into one. Rename the offending key(s)."
+  }
+}
